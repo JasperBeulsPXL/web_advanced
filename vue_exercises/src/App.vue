ec187c7be98d05b837f0eb17142dd61a8d95c7d4
Jasper Beuls <jasper.beuls@pxl.be>
2020-03-27 09:03:34 +0100

    Added static data to exercises

--- vue_exercises/src/App.vue
+++ vue_exercises/src/App.vue
@@ -5,10 +5,33 @@
 </template>
 
 <script>
-
 export default {
   name: 'App',
-  components: {
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: 'Kamer opruimen',
+          completed: false
+        }, {
+          id: 2,
+          title: 'Eten maken',
+          completed: false
+        }, {
+          id: 3,
+          title: 'Naar de winkel gaan',
+          completed: false
+        }, {
+          id: 4,
+          title: 'Corona overleven',
+          completed: false
+        }
+      ],
+      lastId: 0
+    }
+  },
+  methods(){
 
   }
 }
